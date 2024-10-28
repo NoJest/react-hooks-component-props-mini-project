ef84718804bff379a9db6dfe955539d891624fe6
JustinpThomasson <Foreveraendeavour@gmail.com>
2024-10-28 15:23:25 -0400

    amrita helped

---
+++ src/components/ArticleList.jsx
@@ -0,0 +1,22 @@
+import Article from "./Article.jsx"
+
+
+
+function ArticleList ({posts}) {
+    
+return (
+    <>
+        <main>
+            { posts.map(article => (
+    <Article   
+    key= {article.id}
+    title={article.title} 
+    date={article.date} 
+    preview={article.preview} 
+    />
+            ))}
+        </main>
+        </>
+    )
+}
+export default ArticleList
